cf1eafd8ed6092bed2add6dd472946a13ba4dc92
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-06-14 22:31:50 -0300

    adding database

---
+++ ContosoPizza/Queries.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Pizzas" ("Id","Name","Size","IsGlutenFree","Price") VALUES (9,'Jam&Arugula', 2, 1, 20.00);
+SELECT * FROM "Pizzas";
+SELECT * FROM "Pizzas" WHERE "IsGlutenFree" = 1;
+SELECT * FROM "Pizzas" WHERE "Size" = 1;
